442906d21ae2bbe4312bc733aee1aa5736ee4601
eveporcello <eve@moonhighway.com>
2020-09-07 15:17:27 -0700

    Adds xstate samples

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useMachine } from "@xstate/react";
+import tahoeTravelMachine from "./tahoeTravelMachine";
+
+const Walking = ({ send, methods }) => (
+  <>
+    {methods.map((method) => (
+      <button key={method} onClick={() => send(method)}>
+        {method}
+      </button>
+    ))}
+  </>
+);
+
+const Hitchhiking = ({ send, methods }) => (
+  <>
+    {methods.map((method) => (
+      <button key={method} onClick={() => send(method)}>
+        {method}
+      </button>
+    ))}
+  </>
+);
+
+const Disappointed = ({ send, methods }) => (
+  <>
+    {methods.map((method) => (
+      <button key={method} onClick={() => send(method)}>
+        {method}
+      </button>
+    ))}
+  </>
+);
+
+const Riding = ({ send, methods }) => (
+  <>
+    {methods.map((method) => (
+      <button key={method} onClick={() => send(method)}>
+        {method}
+      </button>
+    ))}
+  </>
+);
+
+const Arrived = () => <h1>You Made It!</h1>;
 
 function App() {
+  const [current, send] = useMachine(tahoeTravelMachine);
+  const currentState = current.value;
+
+  console.log(
+    current.configuration[0].machine.config.states[currentState].on
+  );
+
+  const methods = current.configuration[0].machine.config.states[
+    currentState
+  ].on
+    ? Object.keys(
+        current.configuration[0].machine.config.states[currentState]
+          .on
+      )
+    : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>You are: {current.value}</h1>
+
+      {current.value !== "arrived" ? (
+        <h2>What's your next move?</h2>
+      ) : null}
+      {currentState === "walking" ? (
+        <Walking send={send} methods={methods} />
+      ) : current.value === "hitchhiking" ? (
+        <Hitchhiking send={send} methods={methods} />
+      ) : current.value === "riding" ? (
+        <Riding send={send} methods={methods} />
+      ) : current.value === "disappointed" ? (
+        <Disappointed send={send} methods={methods} />
+      ) : (
+        <Arrived />
+      )}
     </div>
   );
 }
-
 export default App;
